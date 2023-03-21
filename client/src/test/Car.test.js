27e7b0be8482ec0a6fe08ab335f60af3bcd876d3
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2023-03-21 16:01:50 +0100

    cambios

---
+++ client/src/test/Car.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { createMemoryHistory } from "history";
+import Cart from "../components/Carts/Cart/Cart.jsx";
+import { Provider } from "react-redux";
+import store from "../redux/store/index";
+import { screen, getByLabelText } from "@testing-library/dom";
+import renderer from "react-test-renderer";
+
+describe("App component", () => {
+  const history = createMemoryHistory();
+  let component;
+
+  const item = {
+    id: 1,
+    image: "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg",
+    weight: "3 - 6",
+  };
+
+  beforeEach(() => {
+    component = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Cart item={item} />
+        </Router>
+      </Provider>
+    );
+  });
+
+  // test("renderizar el componente Cart", () => {
+  //   screen.debug();
+  // });
+
+  test("debe mostrar el componente Cart", () => {
+    expect(component.container).toBeInTheDocument();
+  });
+
+  it("changes the class when hovered", () => {
+    const component = renderer.create(
+      <Provider store={store}>
+        <Router history={history}>
+          <Cart item={item} />
+        </Router>
+      </Provider>
+    );
+    
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+
+  });
+});
