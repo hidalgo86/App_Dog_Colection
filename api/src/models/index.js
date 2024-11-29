6bdf011397a80808cd5bc5fef4e4f4db014d4776
Hidalgo <hidalgojose86@gmail.com>
2024-11-29 19:36:25 +0100

    ajuste variable de entorno

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -11,6 +11,7 @@ const db = {};
 
 // Configuracion de conexion a postgres:
 let sequelize;
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === production) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:"postgres",
